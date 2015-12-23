1f0035f8161a11a90b242d241e643eefbeac0c4b
Bruce Doan <rgv151@gmail.com>
2015-12-23 17:34:44 +0700

    almost done

--- cauvong.nim
+++ cauvong.nim
@@ -1,9 +1,8 @@
-import strutils, sequtils
+import macros, strutils
 
 type
   Format = enum
-    RESET_ALL
-    BOLD
+    BOLD = 1
     DIM
     UNDERLINE = 4
     BLINK
@@ -11,36 +10,126 @@ type
     HIDDEN
 
 
-    RED = 31
+    BLACK = 30
+    RED
+    GREEN
+    YELLOW
+    BLUE
+    MAGENTA
+    CYAN
+    LIGHT_GRAY
+    DARK_GRAY = 90
+    LIGHT_RED
+    LIGHT_GREEN
+    LIGHT_YELLOW
+    LIGHT_BLUE
+    LIGHT_MAGENTA
+    LIGHT_CYAN
+    WHITE
 
-  CauVong = object
+
+  CauVong = ref object
     text: string
-    flags: seq[FormatCode]
+    flags: seq[string]
 
-proc newCauVong(s: string, f: FormatCode): CauVong =
+proc add(c: CauVong, f: Format) =
+  c.flags.add($(f.ord))
+
+
+proc newCauVong(s: string, f: Format): CauVong =
+  new(result)
   result.text = s
   result.flags = @[]
-  result.flags.add(f)
+  result.add(f)
 
-proc `$`*(cv: CauVong): string =
+
+proc `$`*(cv: CauVong): string {.inline.} =
   if len(cv.flags) > 0:
     var flags = ""
-
-    flags.add($(cv.flags[0].ord))
+    flags.add(cv.flags[0])
     for i in 1..cv.flags.len-1:
       flags.add(";")
-      flags.add($(cv.flags[i].ord))
-
-    result = "\e[$#m$#\e[0m" % [flags, cv.text]
+      flags.add(cv.flags[i])
+      result = "\e["
+      result.add(flags)
+      result.add("m")
+      result.add(cv.text)
+      result.add("\e[0m")
   else:
     result = cv.text
 
 
-proc bold*[T: string|CauVong](s: T): CauVong =
-  when s is CauVong:
-    result.add(BOLD)
-  else:
-    result = newCauVong(s, BOLD)
+macro genProc(e: expr): stmt {.immediate.} =
+  result = newStmtList()
 
-var a = "Hello World".bold
-echo a
+  var procName = ($e).toLower
+  var ret = !"result"
+  result.add(
+    newProc(
+      name = ident(procName).postfix("*"),
+      params = [
+        ident("CauVong"),
+        newIdentDefs(
+          ident("s"),
+          ident("string")
+        )
+      ],
+      body = newStmtList(newAssignment(
+        ident("result"),
+        newCall("newCauVong", ident("s"), ident($e))
+      ))
+    )
+  )
+  echo result.treeRepr
+  result.add(
+    newProc(
+      name = ident(procName).postfix("*"),
+      params = [
+        ident("CauVong"),
+        newIdentDefs(
+          ident("cv"),
+          ident("CauVong")
+        )
+      ],
+      body = quote do:
+        cv.add(`e`)
+        cv
+    )
+  )
+
+genProc(BOLD)
+genProc(DIM)
+genProc(UNDERLINE)
+genProc(BLINK)
+genProc(INVERTED)
+genProc(HIDDEN)
+
+genProc(BLACK)
+genProc(RED)
+genProc(GREEN)
+genProc(YELLOW)
+genProc(BLUE)
+genProc(MAGENTA)
+genProc(CYAN)
+genProc(LIGHT_GRAY)
+genProc(DARK_GRAY)
+genProc(LIGHT_RED)
+genProc(LIGHT_GREEN)
+genProc(LIGHT_YELLOW)
+genProc(LIGHT_BLUE)
+genProc(LIGHT_MAGENTA)
+genProc(LIGHT_CYAN)
+genProc(WHITE)
+
+dumpTree:
+  proc red*(s: string): CauVong =
+    result = newCauVong(s, RED)
+#  proc red*(cv: CauVong): CauVong =
+#    cv.add(RED)
+#    cv
+
+
+
+when isMainModule:
+  echo "Hello World".red.bold.underline
+  echo "Welcome".red, " to".green, " the".yellow, " world".blue, " of".magenta, " colors".cyan
